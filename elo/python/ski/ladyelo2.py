544f44d46c04c2e7b175c95fef0adff778e1caa0
Sam Long <shlongenbach@wpi.edu>
2020-12-22 00:25:05 -0500

    wiki way

--- elo/python/ski/ladyelo2.py
+++ elo/python/ski/ladyelo2.py
@@ -1,35 +1,40 @@
 import pandas as pd
 import numpy as np
 
+K=1
 ladiesdf = pd.read_pickle("~/ski/elo/python/ski/ladiesdf.pkl")
 
-mendf = pd.read_pickle("~/ski/elo/python/ski/mendf.pkl")
+def EA (pelos, index):
+    players = len(pelos)
+    ra = pelos[index]
+    QA = 10**(ra/400) 
+    QA = np.repeat(QA, players - 1)
+    rb = np.delete(np.array(pelos), index)
+    QB = 10**(rb/400)
+    EA = QA / (QA + QB)
+    return EA
 
-import numpy as np
-K=1
-
-def EA (pelos ,place):
-	players = len(pelos)
-	ra = pelos[place - 1]
-	QA = 10**(ra/400) 
-	QA = np.repeat(QA, players - 1)
-
-	rb = np.delete(np.array(pelos), place - 1)
-	QB = 10**(rb/400)
-	EA = QA / (QA + QB)
-	return EA
-
-def SA (pelos,place):
-	players = len(pelos)
-	return (place - 1)*[0] + (players-place)*[1]
-
+def SA(place_vector, place):
+    place_vector = np.array(place_vector)
+    losses = (place_vector < place).sum()
+    draws = np.count_nonzero(place_vector == place) - 1
+    wins = (place_vector > place).sum()
+    return losses*[0] + draws*[0.5] + wins*[1]
 
 def lady_elo():
-	ladiesdf = pd.DataFrame()
+	#Step 1: Figure out the person's previous elo.  
+	#If they aren't in the new dataframe, make them a new score (1300)
+	ladieselodf = pd.DataFrame()
+	#ladieselodf.columns = ['date', 'city', 'country', 'level', 'sex', 'distance', 
+	#'discipline', 'place', 'name', 'nation','season','race', 'pelo', 'elo']
+
 	name_pool = []
-	seasons = pd.unique(ladiesdf['season'])
-	#for season in range(len(seasons)):
-	for season in range(1):
+	#Print the unique seasons
+	seasons = (pd.unique(ladiesdf['season']))
+	for season in range(len(seasons)):
+	#for season in range(10):
+		print(seasons[season])
+
 		seasondf = ladiesdf.loc[ladiesdf['season']==seasons[season]]
 		races = pd.unique(seasondf['race'])
 		for race in range(len(races)):
@@ -47,14 +52,32 @@ def lady_elo():
 					vetskier = ladieselodf.loc[ladieselodf['name']==racedf['name'][a]]
 					pelo_list.append(vetskier['elo'].iloc[-1])
 
-
 				#else we have to find them and see if they are the same person
 				#else we have to find their last elo in ladieselodf
 			racedf['pelo'] = pelo_list
-			for b in range(len(pelo_list)):
-				print(pelo_list[b])
-				place = b+1
-				print(pelo_list[place-1] + K*(sum(SA(pelo, place)) - sum(EA(pelo,place))))
+			PLACES = list(racedf['place'])
+            for i,p in enumerate(PLACES):
+                elo_list.append(pelo_list[i] + K*(sum(SA(PLACES, PLACES[i])) - sum(EA(pelo_list,i))))
+
+			racedf['elo'] = elo_list
+			ladieselodf = ladieselodf.append(racedf)
+			#print(ladieselodf)
+		endseasondate = int(str(seasons[season])+'0500')
+		#print(endseasondate)
+		for n in range(len(name_pool)):
+			endskier = ladieselodf.loc[ladieselodf['name']==name_pool[n]]
+			endpelo = endskier['elo'].iloc[-1]
+			endelo = endpelo*.85+1300*.15
+			endnation = endskier['nation'].iloc[-1]
+			endf = pd.DataFrame([[endseasondate, "Summer", "Break", "end", "L", 0, None, 0
+				, name_pool[n], endnation, seasons[season], 0, endpelo, endelo]], columns = ladieselodf.columns)
+			ladieselodf = ladieselodf.append(endf)
+
+	return ladieselodf	
+
+ladieselodf = lady_elo()
+ladieselodf.to_pickle("~/ski/elo/python/ski/ladieselodf2.pkl") #changed
+ladieselodf.to_excel("~/ski/elo/python/ski/ladieselodf2.xlsx") #changed
 
 
-print(lady_elo())
+#def male_elo():
